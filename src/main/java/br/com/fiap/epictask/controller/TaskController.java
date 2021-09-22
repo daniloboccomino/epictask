d16ac34cfd11b482d3d1e8857830e99e9b44dc45
daniloboccomino <daniloboccomino@gmail.com>
2021-09-21 22:26:10 -0300

    configurando rota de tasks

---
+++ src/main/java/br/com/fiap/epictask/controller/TaskController.java
@@ -0,0 +1,15 @@
+package br.com.fiap.epictask.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // determina a classe como Controller para mapeamento de rotas
+public class TaskController {
+
+	// metodo de retorno da pagina index da rota de tasks
+	@RequestMapping("/tasks") // define a rota que chama a pagina
+	public String index() {
+		return "tasks"; // nome do arquivo html que deve ser renderizado (resources/templates/tasks.html)
+	}
+
+}
