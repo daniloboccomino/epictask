8933604896a99b7f97e19d5777d55ef729c59db5
daniloboccomino <daniloboccomino@gmail.com>
2021-09-22 00:00:57 -0300

    adicionando a camada model de task

---
+++ src/main/java/br/com/fiap/epictask/model/Task.java
@@ -0,0 +1,81 @@
+package br.com.fiap.epictask.model;
+
+public class Task {
+	
+	private String title;
+	private String description;
+	private int points;
+	
+	public Task() {}
+	
+	public Task(String title, String description, int points) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.points = points;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [title=" + title + ", description=" + description + ", points=" + points + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + points;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (points != other.points)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+}
