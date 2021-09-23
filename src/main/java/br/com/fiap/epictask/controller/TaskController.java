75b70da033449eb3f0888c266d1384193507ecb9
daniloboccomino <daniloboccomino@gmail.com>
2021-09-22 23:01:50 -0300

    consulta e retorno com ModelAndView

--- src/main/java/br/com/fiap/epictask/controller/TaskController.java
+++ src/main/java/br/com/fiap/epictask/controller/TaskController.java
@@ -1,10 +1,13 @@
 package br.com.fiap.epictask.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.epictask.model.Task;
 import br.com.fiap.epictask.repository.TaskRepository;
@@ -17,8 +20,11 @@ public class TaskController {
 
 	// metodo de retorno da pagina index da rota de tasks
 	@GetMapping("/tasks") // define a rota que chama a pagina
-	public String index() {
-		return "tasks"; // nome do arquivo html que deve ser renderizado (resources/templates/tasks.html)
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("tasks"); // nome do arquivo html que deve ser renderizado (resources/templates/tasks.html)
+		List<Task> tasks = repository.findAll();
+		modelAndView.addObject("tasks", tasks); // objeto renderizado na view
+		return modelAndView;
 	}
 	
 	@PostMapping("/tasks")
