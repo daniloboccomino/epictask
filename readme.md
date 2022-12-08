11f689df8f931d380050a82521ad886a224e3247
daniloboccomino <daniloboccomino@gmail.com>
2022-12-08 19:23:30 -0300

    create project using Spring Initializr

---
+++ readme.md
@@ -0,0 +1,3 @@
+# EPIC TASK
+
+App de controle de tarefas  
---
+++ src/test/java/br/com/daniloboccomino/epictask/EpictaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniloboccomino.epictask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpictaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
