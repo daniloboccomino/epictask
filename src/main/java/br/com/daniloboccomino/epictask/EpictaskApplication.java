9559daf02952c190f71014158491fefadf61d60a
daniloboccomino <daniloboccomino@gmail.com>
2022-12-13 19:19:57 -0300

    add springfox swagger ui

--- src/main/java/br/com/daniloboccomino/epictask/EpictaskApplication.java
+++ src/main/java/br/com/daniloboccomino/epictask/EpictaskApplication.java
@@ -2,8 +2,10 @@ package br.com.daniloboccomino.epictask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class EpictaskApplication {
 
 	public static void main(String[] args) {
